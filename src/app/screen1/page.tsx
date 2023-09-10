fc5c9ded7dd85b0ac7769d0e2910eb402e0800c1
david-dom <113909984+david-dom@users.noreply.github.com>
2023-09-10 18:50:42 -0500

    My commit message

---
+++ src/app/screen1/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from './screen1.module.css';
+import { Metadata } from "next";
+
+
+
+export const metadata: Metadata = {
+                
+}
+
+function Screen1() {
+    
+    
+    
+
+    return(
+        <div className={styles["Screen1"]}>
+            
+<div id="layer-1-2c1effc99c9f452aa44b14bca3bddb70" className={styles["layer-1-2c1effc99c9f452aa44b14bca3bddb70"]}    ></div>
+        </div>
+    );
+}
+
+export default Screen1;
+            
